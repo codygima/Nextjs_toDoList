6cdfbc89685e9069b5c2e05cbf2ad9ea95c7ff65
Cody Gima <157744005+codygima@users.noreply.github.com>
2024-07-05 13:19:00 -1000

    Add files via upload

--- toDoList-project/src/App.jsx
+++ toDoList-project/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { EnterItem } from "./components/EnterItem";
+import { ItemList } from "./components/ItemList";
+import "./style.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [toDos, setToDos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    return localValue ? JSON.parse(localValue) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(toDos));
+  }, [toDos]);
+
+  function addToDo(title) {
+    setToDos((currentToDos) => [
+      ...currentToDos,
+      { id: crypto.randomUUID(), title, completed: false },
+    ]);
+  }
+
+  function toggleToDo(id, completed) {
+    setToDos((currentToDos) =>
+      currentToDos.map((toDo) =>
+        toDo.id === id ? { ...toDo, completed } : toDo
+      )
+    );
+  }
+
+  function removeToDo(id) {
+    setToDos((currentToDos) => currentToDos.filter((toDo) => toDo.id !== id));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app-container">
+      <h1 className="todo-header">To-Do List (Using React)</h1>
+      <EnterItem onSubmit={addToDo} />
+      <ItemList toDos={toDos} toggleToDo={toggleToDo} removeToDo={removeToDo} />
+    </div>
+  );
 }
-
-export default App
+``;
